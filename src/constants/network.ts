e1940d6bd0b3dd0b8d6dd341a9c01e16e96c0d1c
Wagalidoom <lukas-8@live.fr>
2025-08-29 14:06:52 +0200

    feat: add network config and type

---
+++ src/constants/network.ts
@@ -0,0 +1,15 @@
+import { gnosis } from 'wagmi/chains';
+import { NetworkConfig } from '../types/network';
+
+export const NETWORK_CONFIG: Record<number, NetworkConfig> = {
+    [gnosis.id]: {
+        ...gnosis,
+        circlesRpcUrl: 'https://rpc.aboutcircles.com/',
+        pathfinderUrl: 'https://pathfinder.aboutcircles.com',
+        profileServiceUrl: 'https://rpc.aboutcircles.com/profiles/',
+        v1HubAddress: '0x29b9a7fbb8995b2423a71cc17cf9810798f6c543',
+        v2HubAddress: '0xc12C1E50ABB450d6205Ea2C3Fa861b3B834d13e8',
+        nameRegistryAddress: '0xA27566fD89162cC3D40Cb59c87AAaA49B85F3474',
+        migrationAddress: '0xD44B8dcFBaDfC78EA64c55B705BFc68199B56376',
+    },
+};
---
+++ src/types/network.ts
@@ -0,0 +1,11 @@
+import { Address, Chain } from "viem";
+
+export interface NetworkConfig extends Chain {
+    circlesRpcUrl: string;
+    pathfinderUrl: string;
+    profileServiceUrl: string;
+    v1HubAddress: Address;
+    v2HubAddress: Address;
+    nameRegistryAddress: Address;
+    migrationAddress: Address;
+}
