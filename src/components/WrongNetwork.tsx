058ee1075764b9c68f8bde7ad3acbcf7e02852eb
Wagalidoom <lukas-8@live.fr>
2025-09-17 09:40:50 +0200

    style: imrpove wrong network modal on mobile

--- src/components/WrongNetwork.tsx
+++ src/components/WrongNetwork.tsx
@@ -4,13 +4,13 @@ export function WrongNetwork() {
     const { switchChain } = useSwitchChain();
 
     return (
-        <div className="absolute top-0 z-20 w-screen h-screen bg-black/50 flex text-black items-center justify-center">
-            <div className="bg-white p-8 rounded-lg">
+        <div className="absolute top-0 z-20 w-screen h-screen bg-black/50 flex text-black items-end sm:items-center justify-center">
+            <div className="bg-white p-8 rounded-t-lg sm:rounded-lg w-full sm:w-auto">
                 <h1 className="text-2xl font-bold">Wrong Network</h1>
                 <p className="mt-4">Please switch to the correct network to continue.</p>
                 <button
                     onClick={() => switchChain({ chainId: 100 })}
-                    className="mt-4 bg-blue-500 text-white rounded-lg px-4 py-2"
+                    className="btn btn-sm btn-neutral mt-4"
                 >
                     Switch to Gnosis Chain
                 </button>
